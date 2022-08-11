4a3f9c4e1c0e6d97386b3981523400d67c3d83ec
Khan <batyrkhan.akzholov@gmail.com>
2022-08-11 17:10:01 +0200

    FactoryPatternCommit

---
+++ Strategy/Strategy/Strategy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Strategy.Strategies;
+
+namespace Strategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero hero = new("Squidward");
+            hero.Attack();
+
+            hero.SetWeapon(new Broom());
+
+            hero.Attack();
+
+
+        }
+    }
+}
---
+++ Strategy/Strategy/Strategy/Strategies/Broom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.Strategies
+{
+    public class Broom : IWeapon
+    {
+        public void Shoot()
+        {
+            Console.WriteLine("attack with Broom");
+        }
+    }
+}
